db9b1374f16b4125f4a2377c00e6cd203816e619
Vsevolod Strukchinsky <floatdrop@users.noreply.github.com>
2015-04-13 10:28:58 +0500

    Accept falsy values
    Closes #2

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ var uuid = require('node-uuid');
 module.exports = function (options) {
     options = options || {};
     options.uuidVersion = options.uuidVersion || 'v4';
-    options.setHeader = options.setHeader !== false;
 
     return function (req, res, next) {
         req.id = uuid[options.uuidVersion](options, options.buffer, options.offset);
-        if (options.setHeader) {
+        if (!!options.setHeader) {
             res.setHeader('X-Request-Id', req.id);
         }
         next();
