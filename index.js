67efd38c61300dffa33091b0895a7d2ab5f1cbf5
Joshua <joshua.evans@quantified.co>
2017-03-14 15:09:55 +0000

    Make the id attribute name configurable (#7)
    * Make id attribute name dynamic
    * Add test for dynamic attribute name
    * Add attribute name to readme
    * Update test name

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ module.exports = function (options) {
     options.uuidVersion = options.uuidVersion || 'v4';
     options.setHeader = options.setHeader === undefined || !!options.setHeader;
     options.headerName = options.headerName || 'X-Request-Id';
+    options.attributeName = options.attributeName || 'id';
 
     return function (req, res, next) {
-        req.id = req.header(options.headerName) || uuid[options.uuidVersion](options, options.buffer, options.offset);
+        req[options.attributeName] = req.header(options.headerName) || uuid[options.uuidVersion](options, options.buffer, options.offset);
         if (options.setHeader) {
-            res.setHeader(options.headerName, req.id);
+            res.setHeader(options.headerName, req[options.attributeName]);
         }
         next();
     };
