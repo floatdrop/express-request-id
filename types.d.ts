106d7e11c85ed399f21802f2bdd5245f08cd4873
Vsevolod Strukchinsky <floatdrop@users.noreply.github.com>
2022-01-16 12:06:43 +0500

    Add right type definitions to package (#19)
    Co-authored-by: Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>

--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import {Request, RequestHandler} from 'express-serve-static-core';
 
 declare global {
 	namespace Express {
@@ -8,3 +9,14 @@ declare global {
 		}
 	}
 }
+
+declare namespace expressRequestId {
+	interface Options {
+		setHeader?: boolean | undefined;
+		headerName?: string | undefined;
+		generator?: ((request: Request) => string) | undefined;
+	}
+}
+
+declare function expressRequestId(options?: expressRequestId.Options): RequestHandler;
+export = expressRequestId;
